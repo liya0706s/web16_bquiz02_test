b0fd22438e67d1b3275781acda6d2e0b17199c37
Angie <liya0706s@gmail.com>
2024-02-25 22:15:16 +0800

    feat:front/login的href是?do=login, 完成html加上js的註解!

--- front/login.php
+++ front/login.php
@@ -11,13 +11,49 @@
         </tr>
         <tr>
             <td>
-                <button onclick="login()">登入</button>
-                <button onclick="clean()">清除</button>
+                <input type="button" value="登入" onclick="login()">
+                <input type="reset" value="清除" onclick="clean()">
             </td>
             <td>
-                <a href="../front/forget.php">忘記密碼</a>
-                <a href="../front/reg.php">尚未註冊</a>
+                <a href="?do=forget">忘記密碼</a>
+                <a href="?do=reg">尚未註冊</a>
             </td>
         </tr>
     </table>
 </fieldset>
+<script>
+    /**
+    登入的函式
+    1. 取得帳號密碼的值
+    2. 發送POST請求到chk_acc.php檢查帳號是否正確
+    3. 如果回傳的值是0代表:查無此帳號
+    
+    4. 否則(帳號正確)再發送POST請求到chk_pw.php檢查密碼是否正確
+    5. 如果回傳值是1代表密碼正確
+    6. 如果帳號值是'admin'代表是管理者, 會導向back.php
+    7. 否則導向首頁
+    8. 判斷密碼不是1的否則，彈出視窗:密碼錯誤
+     */
+
+    function login() {
+        let acc = $("#acc").val()
+        let pw = $("#pw").val()
+        $.post('./api/chk_acc.php',{acc},(res)=>{
+            if(parseInt(res)==0){
+                alert("查無帳號");
+            }else{
+                $.post('./api/chk_pw.php',{acc,pw},(res)=>{
+                    if(parseInt(res)==1){
+                        if($("#acc").val()=='admin'){
+                            location.href="back.php"
+                        }else{
+                            location.href="index.php"
+                        }
+                    }else{
+                        alert("密碼錯誤")
+                    }
+                })
+            }
+        })
+    }
+</script>
