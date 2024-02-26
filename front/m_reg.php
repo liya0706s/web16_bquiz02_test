96f9234ac57a0b3b411595ec38bcc1da8cafdf9e
Angie <liya0706s@gmail.com>
2024-02-26 19:43:11 +0800

    fix:發送post, api網址之後要放上逗號

--- front/m_reg.php
+++ front/m_reg.php
@@ -3,19 +3,19 @@
     <span style="color:red">*請設定您要註冊的帳號及密碼(最長12個字元)</span>
     <table>
         <tr>
-            <td class="clo">帳號:</td>
+            <td class="clo">Step1:登入帳號</td>
             <td><input type="text" name="acc" id="acc"></td>
         </tr>
         <tr>
-            <td class="clo">密碼:</td>
+            <td class="clo">Step2:登入密碼</td>
             <td><input type="password" name="pw" id="pw"></td>
         </tr>
         <tr>
-            <td class="clo">確認密碼:</td>
+            <td class="clo">Step3:再次確認密碼</td>
             <td><input type="password" name="pw2" id="pw2"></td>
         </tr>
         <tr>
-            <td class="clo">信箱:</td>
+            <td class="clo">Step4:信箱(忘記密碼時使用)</td>
             <td><input type="text" name="email" id="email"></td>
         </tr>
         <tr>
@@ -29,7 +29,11 @@
 </fieldset>
 
 <script>
+    // 檢查帳號是否已被使用，
+    // 確認申請的帳號在資料表中是不重覆的，
+    // 才會把表單資料送到後端去進行資料表新增會員資料的動作
     function reg() {
+        // 取得使用者輸入的帳號、密碼、確認密碼和電子信箱
         let user = {
             acc: $("#acc").val(),
             pw: $("#pw").val(),
@@ -37,14 +41,19 @@
             email: $("#email").val()
         }
 
+        // 檢查使用者輸入的資訊是否完整
         if (user.acc != '' && user.pw != '' && user.pw2 != '' && user.email != '') {
+            // 檢查密碼和確認密碼是否相符
             if (user.pw == user.pw2) {
+                // 發送 POST 請求檢查帳號是否重覆
                 $.post("./api/chk_acc.php", {
                     acc: user.acc
                 }, (res) => {
+                    // 如果回傳值的結果為1,表示帳號重覆
                     if (parseInt(res) == 1) {
-                        alert("帳號重複")
+                        alert("帳號重覆")
                     } else {
+                        // 發送 POST 請求進行註冊
                         $.post("./api/reg.php", user, (res) => {
                             alert("註冊完成，歡迎加入")
                         })
@@ -56,8 +65,5 @@
         } else {
             alert("不可空白")
         }
-
-
-
     }
 </script>
